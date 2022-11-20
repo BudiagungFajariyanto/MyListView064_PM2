66c21800d6c6dcbe5edfd48d64b5aa4dfda63c56
Acer <budiagungfajariyanto@gmail.com>
2022-11-20 17:33:41 +0700

    Initial commit

---
+++ app/src/main/java/com/budiagungfajariyanto_f55121064/mylistview064_pm2/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.budiagungfajariyanto_f55121064.mylistview064_pm2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
